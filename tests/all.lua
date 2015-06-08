d1f92105388a2fb3b06577da8ac2244b67c000e6
Lukas Borin <kurtyan@student.chalmers.se>
2015-06-08 20:31:46 +0200

    Removed the old tests.

--- tests/all.lua
+++
@@ -1,7 +0,0 @@
-dofile("tests/dynamictests.lua")
-dofile("tests/encodingtests.lua")
-dofile("tests/monsterexampletest.lua")
-dofile("tests/objecttests.lua")
-dofile("tests/primitivetests.lua")
-dofile("tests/standardtests.lua")
-dofile("tests/typreftests.lua")
