9c17c27096bec8117f2bac786d4384c3d8377dea
TheFlyingFiddle <kurtyan@student.chalmers.se>
2015-08-12 03:53:06 +0200

    Added preserving dynamic type to the standard module.

--- tier/standard.lua
+++ tier/standard.lua
@@ -5,5 +5,5 @@ require"tier.standard.containers"(standard)
 require"tier.standard.behaviour"(standard)
 require"tier.standard.dynamic"(standard)
 require"tier.standard.closure"(standard)
-
+require"tier.standard.preserving"(standard)
 return standard
